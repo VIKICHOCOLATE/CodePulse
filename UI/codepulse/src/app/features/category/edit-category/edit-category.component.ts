bfd60d631e2b4da0c2bfef8f2990318c4ae90bf4
vsuponenka <viktoriasuponenko@gmail.com>
2024-04-12 22:06:17 +0200

    feat(editCategory): added getCategoryById

--- UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
+++ UI/codepulse/src/app/features/category/edit-category/edit-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -11,8 +13,10 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
 
   id: string | null = null;
   paramsSubscription?: Subscription;
+  category?: Category;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+    private categoryService: CategoryService) {
 
   }
   ngOnDestroy(): void {
@@ -22,8 +26,22 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
     this.paramsSubscription = this.route.paramMap.subscribe({
       next: (params) => {
         this.id = params.get('id');
+
+        if (this.id) {
+          // get the data from the API
+          this.categoryService.getCategoryById(this.id)
+          .subscribe({
+            next: (response) => {
+              this.category = response;
+            }
+          });
+
+        }
       }
     })
   }
 
+  onFormSubmit(): void {
+    console.log(this.category);
+  }
 }
--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -22,4 +22,8 @@ export class CategoryService {
     return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
 
   }
+
+  getCategoryById(id: string): Observable<Category> {
+    return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`);
+  }
 }
